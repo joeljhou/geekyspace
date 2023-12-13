0994d95b24733a26987044c674fd9c3b036e03e9
zhouyu <zhouyu@liquido.cn>
2023-12-14 03:11:32 +0800

    导航栏配置navbar优化

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,10 +2,80 @@ import {navbar} from "vuepress-theme-hope";
 
 export default navbar([
     /*首页*/
-    "/",
+    {
+        text: "首页",
+        icon: "home",
+        link: "/",
+    },
     /*所有文章*/
-    "/timeline",
-
+    {
+        text: "所有文章",
+        icon: "list",
+        link: "/timeline",
+    },
+    /*Java教程*/
+    {
+        text: "Java教程",
+        // icon: "java",
+        children: [
+            {
+                text: "Java 8 教程",
+                icon: "java",
+                link: "https://books.didispace.com/java8-tutorial/index.html",
+            },
+            {
+                text: "Java 新特性",
+                icon: "java",
+                link: "/java-features",
+            },
+        ]
+    },
+    /*Spring教程*/
+    {
+        text: "Spring教程",
+        // icon: "spring",
+        children: [
+            {
+                text: "Spring Boot 教程",
+                icon: "spring",
+                link: "/spring-boot",
+            },
+            {
+                text: "Spring Cloud 教程",
+                icon: "spring",
+                link: "/spring-cloud",
+            },
+            {
+                text: "Spring Cloud Alibaba 教程",
+                icon: "spring",
+                link: "/spring-cloud-alibaba",
+            },
+        ]
+    },
+    /*安装大全*/
+    {
+        text: "安装大全",
+        icon: "launch",
+        link: "/installation-guide",
+    },
+    /*玩转IDEA*/
+    {
+        text: "玩转IDEA",
+        icon: "intellij-idea",
+        link: "/idea-tips",
+    },
+    /*文库汇总*/
+    {
+        text: "文库汇总",
+        icon: "article",
+        link: "/home",
+    },
+    /*加群交流*/
+    {
+        text: "加群交流",
+        icon: "wechat",
+        link: "/wechat",
+    },
     "/demo/",
     {
         text: "博文",
@@ -47,10 +117,5 @@ export default navbar([
             "tomato",
             "strawberry",
         ],
-    },
-    {
-        text: "V2 文档",
-        icon: "book",
-        link: "https://theme-hope.vuejs.press/zh/",
     },
 ]);
