bf3b75dbc36e4a648fa20ade7dc399f4e503eb9c
zhouyu <zhouyu@liquido.cn>
2023-12-18 00:34:20 +0800

    部署路径指定

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/joeljhou/",
 
   lang: "zh-CN",
   title: "会敲代码的程序猿",
