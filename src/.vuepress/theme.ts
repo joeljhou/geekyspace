bf34cee6f0f96566c6d2261801aaf14f8bb2ada2
zhouyu <zhouyu@liquido.cn>
2023-12-19 00:03:51 +0800

    修改个人头像

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -21,7 +21,7 @@ export default hopeTheme({
   /* 使用阿里妈妈Iconfont矢量图标，日后添加了新的图标，请重新生成新的 CSS 地址并替换 */
   iconAssets: "//at.alicdn.com/t/c/font_4370612_23wa7yckp7fh.css",
 
-  logo: "head.png",
+  logo: "IMG_HEAD.PNG",
 
   repo: "https://github.com/joeljhou",
 
