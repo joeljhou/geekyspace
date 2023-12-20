1edcbecb636054715c888145c2c13d1eac1783e0
zhouyu <zhouyu@liquido.cn>
2023-12-20 17:42:30 +0800

    分组侧边栏配置规范案例

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -2,31 +2,16 @@ import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
     "/": [
-        /*最新发布*/
-        "/",
         /*如何使用*/
-        {
-            text: "如何使用",
-            icon: "laptop-code",
-            prefix: "demo/",
-            link: "demo/",
-            children: "structure",
-        },
+        {text: "如何使用", icon: "laptop-code", prefix: "demo/", link: "demo/", children: "structure",},
         /*文章*/
-        {
-            text: "文章",
-            icon: "book",
-            prefix: "posts/",
-            children: "structure",
-        },
-        "intro",
-        "slides",
+        {text: "文章", icon: "book", prefix: "posts/", children: "structure",},
     ],
     /*Java新特性*/
-    "/java-features": [
-        {text: "总目录", link: "/java-features"},
+    "/java-features/": [
+        {text: "总目录", prefix: "/java-features/", link: "/java-features/",},
         {
-            text: "Java 9", link: "/java-features/java9/", prefix: "/java-features/java9/",
+            text: "Java 9", prefix: "Java9/", link: "Java9/",
             children: [
                 {text: "交互式编程环境JShell", link: "jep222-jshell"},
                 {text: "不可变集合的快捷创建方法", link: "jep269-convenience-factory-methods-for-collections"},
