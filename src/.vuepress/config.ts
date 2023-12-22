755953b863f24621a603c263756b2a09a919f225
zhouyu <zhouyu@liquido.cn>
2023-12-22 14:44:54 +0800

    实现搜索插件功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,5 @@
-import { defineUserConfig } from "vuepress";
+import {defineUserConfig} from "vuepress";
+import {searchProPlugin} from "vuepress-plugin-search-pro";
 import theme from "./theme.js";
 
 export default defineUserConfig({
@@ -9,6 +10,22 @@ export default defineUserConfig({
   description: "vuepress-theme-hope 的博客搭建",
 
   theme,
+
+  plugins: [
+    searchProPlugin({
+      indexContent: true,  // 索引全部内容
+      customFields: [      // 为分类和标签添加索引
+        {
+          formatter: "分类：$content",
+          getter: (page) => page.frontmatter.category as string | string[] | null,
+        },
+        {
+          formatter: "标签：$content",
+          getter: (page) => page.frontmatter.tag as string | string[] | null,
+        },
+      ],
+    }),
+  ],
 
   // Enable it with pwa
   // shouldPrefetch: false,
