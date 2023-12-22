a2cae363fc5e54341cd1034fc7c4d4d759eb2696
zhouyu <zhouyu@liquido.cn>
2023-12-22 16:19:08 +0800

    searchProPlugin官网写法部署测试

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -13,15 +13,17 @@ export default defineUserConfig({
 
   plugins: [
     searchProPlugin({
-      indexContent: true,  // 索引全部内容
-      customFields: [      // 为分类和标签添加索引
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
         {
+          getter: (page) => page.frontmatter.category,
           formatter: "分类：$content",
-          getter: (page) => page.frontmatter.category as string | string[] | null,
         },
         {
+          getter: (page) => page.frontmatter.tag,
           formatter: "标签：$content",
-          getter: (page) => page.frontmatter.tag as string | string[] | null,
         },
       ],
     }),
